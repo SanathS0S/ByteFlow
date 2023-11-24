ac3f017734fa3e2a2d7d5fb25d5656aed253c2e5
Sanath S <95021339+SanathS0S@users.noreply.github.com>
2023-11-24 21:34:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import join_room, leave_room, send, SocketIO,emit
+from flask_socketio import join_room, leave_room, send, SocketIO, emit
 import random
 from string import ascii_uppercase, digits
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "CNPROJECT"
-socketio = SocketIO(app)
 rooms = {}
+
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=100000000)
+
 
 def generate():
     letters = ascii_uppercase + digits
     room_code = ''.join(random.choice(letters) for _ in range(6))
     return room_code
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -21,7 +24,7 @@ def home():
         code = request.form.get("id")
         action = request.form.get("action")
 
-        if not name and action=="join":
+        if not name and action == "join":
             return render_template("home.html", error="Please enter a name", code=code, name=name)
         if action == "join" and not code:
             return render_template("home.html", error="Please enter a room code", code=code, name=name)
@@ -40,12 +43,14 @@ def home():
 
     return render_template("home.html")
 
+
 @app.route("/room")
 def room():
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
     return render_template("room.html", code=room)
+
 
 @socketio.on("message")
 def message(data):
@@ -59,13 +64,30 @@ def message(data):
     send(content, to=room)
     rooms[room]["messages"].append(content)
 
+
 @socketio.on('file_shared')
 def handle_file_shared(data):
     filename = data['filename']
     file_content = data['file_content']
-    
+    filetype = data['filetype']
+    name = session.get("name")
     # Broadcast the file to all connected clients, including the sender
-    emit('file_received', {'filename': filename, 'file_content': file_content}, broadcast=True)
+    emit('file_received', {'filename': filename, 'file_content': file_content,'name':name,filetype:filetype}, broadcast=True)
+
+@socketio.on('audio_shared')
+def handle_file_shared(data):
+    filename = data['filename']
+    file_content = data['file_content']
+    filetype = data['filetype']
+    name = session.get("name")
+    # Broadcast the file to all connected clients, including the sender
+    emit('audio_received', {'filename': filename, 'file_content': file_content,'name':name,filetype:filetype}, broadcast=True)
+@socketio.on('image_shared')
+def handle_image(img_data):
+    name = session.get("name")
+    # Broadcast the image to all connected clients, including the sender
+    emit('image_received',img_data, broadcast=True)
+
 
 @socketio.on("connect")
 def connect(auth):
@@ -79,7 +101,7 @@ def connect(auth):
     join_room(room)
     send({"name": name, "message": "has entered the room"}, to=room)
     rooms[room]["members"] += 1
-    #print(f"{name} joined room {room}")
+
 
 @socketio.on("disconnect")
 def disconnect():
@@ -93,5 +115,6 @@ def disconnect():
             del rooms[room]
     send({"name": name, "message": "has left the room"}, to=room)
 
+
 if __name__ == "__main__":
-   socketio.run(app, host='127.0.0.1', port=8080, debug=True)
+    socketio.run(app,debug=True)
